e7d897fc05d4123450f5433918a2abdee51a76f9
lesley paone <lesley@dh42.com>
2014-11-24 10:17:45 -0600

    Validation change

--- controllers/admin/AdminReOccurOrdersController.php
+++ controllers/admin/AdminReOccurOrdersController.php
@@ -1019,7 +1019,7 @@ class AdminReOccurOrdersController extends ModuleAdminController
 				$payment_module->validateOrder(
 					(int)$cart->id, (int)$id_order_state,
 					$cart->getOrderTotal(true, Cart::BOTH), $payment_module->displayName, $this->l('Manual order -- Employee:').' '.
-					substr($employee->firstname, 0, 1).'. '.$employee->lastname, array(), null, false, $cart->secure_key
+					Tools::substr($employee->firstname, 0, 1).'. '.$employee->lastname, array(), null, false, $cart->secure_key
 				);
 				if ($payment_module->currentOrder)
 					Tools::redirectAdmin(self::$currentIndex.'&id_order='.$payment_module->currentOrder.'&vieworder'.'&token='.$this->token);
